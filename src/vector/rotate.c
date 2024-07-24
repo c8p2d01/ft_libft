7c02565964640b3b2a1aaa43419a7a1e54711e5f
c8p2d01 <cdahlhof@42wolfsbug.de>
2024-07-24 19:55:42 +0200

    wrong angle conversion fixed

--- src/vector/rotate.c
+++ src/vector/rotate.c
@@ -5,7 +5,7 @@ vec2d_t		rotate2d(vec2d_t *vector, double degree)
 	vec2d_t	result;
 	double	rad;
 	
-	rad = degree * 360 / PI;
+	rad = degree * PI / 180;
 	result.x = vector->x * cos(rad) - vector->y * sin(rad);
 	result.y = vector->x * sin(rad) + vector->y * cos(rad);
 	//normalise2d(&result);
