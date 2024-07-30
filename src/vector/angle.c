865ea8328f88b69b4d5042eff67bbc62d6d1764b
c8p2d01 <cdahlhof@42wolfsbug.de>
2024-07-30 09:59:13 +0200

    forgot part pf the formula

--- src/vector/angle.c
+++ src/vector/angle.c
@@ -2,14 +2,11 @@
 
 double	angle2d(vec2d_t a, vec2d_t b)
 {
-	return (((a.x * b.x) + (a.y * b.y)) \
-			/ \
-			(veclen2d(&a) * veclen2d(&b)));
+	return (acos(((a.x * b.x) + (a.y * b.y)) / (veclen2d(&a) * veclen2d(&b))));
 }
 
 double	angle3d(vec3d_t a, vec3d_t b)
 {
-	return (((a.x * b.x) + (a.y * b.y) + (a.z * b.z)) \
-			/ \
-			(veclen3d(&a) * veclen3d(&b)));
+	return (acos(((a.x * b.x) + (a.y * b.y) + (a.z * b.z)) / \
+				(veclen3d(&a) * veclen3d(&b))));
 }
