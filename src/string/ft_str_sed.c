a679a4850ec2d902458bdb0843228969ed6d9615
c8p2d01 <cdahlhof@42wolfsbug.de>
2024-08-25 06:27:33 +0200

    not norm but compiling?

--- src/string/ft_str_sed.c
+++ src/string/ft_str_sed.c
@@ -28,9 +28,12 @@ void	str_sed(char **base, char *ind, char *add)
 	len[1] = ft_strlen(add);
 	scour = *base;
 	n = -1;
-	while (scour - 1 != 0)
+	while (scour != 0)
 	{
-		scour = ft_strnstr(scour, ind, ft_strlen(scour)) + 1;
+		scour = ft_strnstr(scour, ind, ft_strlen(scour));
+		if (!scour)
+			break ;
+		scour += len[1];
 		n++;
 	}
 	*base = ft_realloc(*base, ft_strlen(*base), n * (len[1] - len[0]));
@@ -45,46 +48,3 @@ void	str_sed(char **base, char *ind, char *add)
 		scour += len[1];
 	}
 }
-
-// int main()
-// {
-// 	char	*frame = {"\
-// 		~~~~~~~~~~~~~~~^!&@@@@@&!^~~~~~~~~~~~~~~~~~~~~~~~\n\
-// 		~~~~~~~~~~~~~7PPPGGGGGGGJPP7~~~~~~~~~~~~~~~~~~~~~\n\
-// 		~~~~~~~~~~~~^P@@&.     .&@@P^~~~~~~~~~~~~~~~~~~~~\n\
-// 		~~~~~~~~~!PPPYPP7:::::::Y555PPPP77!~~~~~~~~~~~~~~\n\
-// 		~~~~~~~~~7@@@7   ^^^^^^^!!~5@@@YPP7~~~~~~~~~~~~~~\n\
-// 		~~~~~~~~^7@@@P   :::::::!7!5@@@YPPPPPP!~~~~~~~~~~\n\
-// 		~~~~~~7PPJGGG!......^7!!!!~J#BB5Y5YPPP!~~~~~~~~~~\n\
-// 		~~~~~~G@@B   :^^^   J@@@!^^~!!!#@@BPPP!~~~~~~~~~~\n\
-// 		~~~~~~G@@B   :^^^   J@@&!^^~!!!#@@BPPP!~~~~~~~~~~\n\
-// 		~~~~~~G@@B   :^^^   J@@&!^^~!!!#@@BPPP!~~~~~~~~~~\n\
-// 		~~~~~~G@@B   :^^^   J@@&!^^~!!!#@@BPPPPPP7~~~~~~~\n\
-// 		~~~~~~G@@B   :^^^   J@@&!^^~!!!#@@BPPPPPP7~~~~~~~\n\
-// 		~~~~~~G@@B   :^^^   J@@&!^^~!!!#@@BPPPPPP7~~~~~~~\n\
-// 		~~~~~~G@@B   :^^^   J@@&!^^~!!!#@@BPPPPPP7~~~~~~~\n\
-// 		~~~~~~G@@B   :^^^   J@@&!^^~!!!#@@BPPPPPP7~~~~~~~\n\
-// 		~~~~~~G@@B   :^^^   J@@&!^^~!!!#@@BPPPPPP7~~~~~~~\n\
-// 		~~~~~~G@@B   :^^^   J@@&!^^~!!!#@@BPPPPPP7~~~~~~~\n\
-// 		~~~~~~G@@B   :^^^   J@@&!^^~!!!#@@BPPPPPP7~~~~~~~\n\
-// 		~~~~~~G@@B   :^^^   J@@@!^^~!!!#@@BPPPPPP7~~~~~~~\n\
-// 		~~~~~~G@@B...:^^^   J@&&!^^~777#@@BPPPPPP7~~~~~~~\n\
-// 		~~~~~~!!!P##&J^^^:..:^^^!!!Y&&&YJJJPPPPPP7~~~~~~~\n\
-// 		~~~~~~~~~7@@@Y^^^:::::::!!!5@@@YPPPPPPPPP7~~~~~~~\n\
-// 		~~~~~~~~~7@@@J:^^^^^^^^^!!!5@@@YPPPPPPPPP7~~~~~~~\n\
-// 		~~~~~~~~~!JJJ5GGP!~~~~~!PGGG555JPPPPPP7!!!~~~~~~~\n\
-// 		~~~~~~~~~~~~^P@@&P77777P&@@G7PPPPPPPPP!~~~~~~~~~~\n\
-// 		~~~~~~~~~~~~~!!!7&@&&&&&YJJJPPPPPPPPPP!~~~~~~~~~~\n\
-// 		~~~~~~~~~~~~~~~~!GGGBBBBJPPPPPPPPPP777!~~~~~~~~~~\n\
-// 		~~~~~~~~~~~~~~~~~~~^!PPPPPPPPPPPPP7~~~~~~~~~~~~~~\n\
-// 		~~~~~~~~~~~~~~~~~~~~!!!!PPPPPPP!!!!~~~~~~~~~~~~~~\n\
-// 		~~~~~~~~~~~~~~~~~~~~~~~~PPPPPPP~~~~~~~~~~~~~~~~~~\n"
-// 	};
-
-// 	char *str = ft_strdup(frame);
-
-// 	printf("before\n%s", str);
-// 	str_sed(&str, "\t\t", "\e[38;2;255;42;42m");
-// 	str_sed(&str, "\n", "\e[0m\n");
-// 	printf("after\n%s", str);
-// }
