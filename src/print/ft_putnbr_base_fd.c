db8f0b4c2d2edb4a0969ccb91cc655a7e54b374a
c8p2d01 <cdahlhof@42wolfsbug.de>
2024-12-19 23:21:18 +0100

    how was this worng in here for so long?

--- src/print/ft_putnbr_base_fd.c
+++ src/print/ft_putnbr_base_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_base_fd.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cdahlhof <cdahlhof@student.42wolfsburg.    +#+  +:+       +#+        */
+/*   By: cdahlhof <cdahlhof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 14:23:42 by cdahlhof          #+#    #+#             */
-/*   Updated: 2023/01/02 15:43:27 by cdahlhof         ###   ########.fr       */
+/*   Updated: 2024/12/19 23:20:45 by cdahlhof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_putnbr_base_fd(unsigned int num, char *base, int fd)
 	int	i;
 
 	i = 1;
-	if (num > ft_strlen(base))
+	if (num >= ft_strlen(base))
 		i += ft_putnbr_base_fd(num / ft_strlen(base), base, fd);
 	write(fd, &(base[(num % ft_strlen(base))]), 1);
 	return (i);
